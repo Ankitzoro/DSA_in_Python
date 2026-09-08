7e906bc82d28a22b7ff76e1931269ce0e75e5696
Ankit Yadav <ankityadavzoro@gmail.com>
2026-09-08 23:38:07 +0530

    Create Segregate 0s and 1s.py

---
+++ Two Pointers/Segregate 0s and 1s.py
@@ -0,0 +1,22 @@
+class Solution:
+    def segregate0and1(self, arr):
+        n=len(arr)
+        left=0
+        right=n-1
+        
+        while left<right:
+            
+            if arr[left]==0 and arr[right]==1:
+                left+=1
+                right-=1
+            
+            elif arr[left]==1 and arr[right]==0:
+                arr[left],arr[right]=arr[right],arr[left]
+                left+=1
+                right-=1
+                
+            elif arr[left]==0 and arr[right]==0:
+                left+=1
+                
+            elif arr[left]==1 and arr[right]==1:
+                right-=1        
