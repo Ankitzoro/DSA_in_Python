4bb381edd01dc3c64a3377ed1d784fa17e735cff
Ankitzoro <ankityadavzoro@gmail.com>
2026-09-09 21:47:11 +0530

    Remove Duplicates from Sorted List

---
+++ Two Pointers/Remove Duplicates from Sorted List.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+class Solution:
+    def deleteDuplicates(self, head):
+        if not head:
+            return None
+
+        curr = head
+
+        while curr.next:
+            if curr.val == curr.next.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+
+        return head
